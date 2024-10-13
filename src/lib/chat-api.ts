d3641e99c4930d9ad026bd31f4b6729ff5bb5e60
Anatoly N <16267644+miloradowicz@users.noreply.github.com>
2024-10-14 03:24:39 +0600

    chat-api updated: api connector returns typed datetime now

--- src/lib/chat-api.ts
+++ src/lib/chat-api.ts
@@ -1,6 +1,9 @@
 import axios from 'axios';
+import qs from 'qs';
 import { DateTime } from 'luxon';
 import { Message } from '../types.d';
+
+type MessageJson = Message & { datetime: string };
 
 const baseUrl = 'http://146.185.154.90:8000';
 
@@ -8,28 +11,31 @@ const headers = {
   Accept: 'application/json',
 };
 
-export const getMessages = async (since?: DateTime) => {
+export const getMessages = async (since?: DateTime): Promise<Message[]> => {
   const endpoint = 'messages';
 
-  const url = new URL(endpoint + (since === undefined ? '' : `?datetime=${since.toISO()}`), baseUrl);
-  const { data, status } = await axios.get<Message[]>(url.href, { headers });
+  const url = new URL(endpoint + (since === undefined ? '' : `?datetime=${since.toUTC().toISO()}`), baseUrl);
+  const { data, status } = await axios.get<MessageJson[]>(url.href, { headers });
 
   if (status !== 200) {
     throw new Error(`${status}`);
   }
 
-  return data;
+  return data.map((x) => ({ ...x, datetime: DateTime.fromISO(x.datetime) }));
 };
 
-export const postMessage = async (author: string, message: string) => {
+export const postMessage = async (author: string, message: string): Promise<Message> => {
   const endpoint = 'messages';
 
   const url = new URL(endpoint, baseUrl);
-  const { data, status } = await axios.post<Message>(url.href, { author, message }, { headers: { ...headers, 'Content-Type': 'application/json' } });
+
+  const { data, status } = await axios.post<MessageJson>(url.href, qs.stringify({ author, message }), {
+    headers: { ...headers },
+  });
 
   if (status !== 200) {
     throw new Error(`${status}`);
   }
 
-  return data;
+  return { ...data, datetime: DateTime.fromISO(data.datetime) };
 };
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,3 +1,12 @@
+import { DateTime } from 'luxon';
+
 export interface MessageFormData {
   get message(): string;
+}
+
+export interface Message {
+  get _id(): string;
+  get message(): string;
+  get author(): string;
+  get datetime(): DateTime;
 }
