3bfb2e10b47d0ef08f3f847af8aa1a22d71e1b1a
Anatoly N <16267644+miloradowicz@users.noreply.github.com>
2024-10-14 03:34:49 +0600

    MessageList added

---
+++ src/components/MessageList/MessageList.tsx
@@ -0,0 +1,26 @@
+import { Message } from '../../types.d';
+import { FC, useEffect, useRef } from 'react';
+import MessageItem from './MessageItem/MessageItem';
+
+interface MessageListProps {
+  messages: Message[];
+}
+
+const MessageList: FC<MessageListProps> = ({ messages }) => {
+  const dummy = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    dummy.current?.scrollIntoView();
+  }, [messages]);
+
+  return (
+    <>
+      {messages.map((x) => (
+        <MessageItem key={x._id} message={x} />
+      ))}
+      <div ref={dummy} />
+    </>
+  );
+};
+
+export default MessageList;
