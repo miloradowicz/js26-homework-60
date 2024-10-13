9495ef967ffa6d4e6ae4a8618aadfbf37631c1ae
Anatoly N <16267644+miloradowicz@users.noreply.github.com>
2024-10-14 05:01:45 +0600

    MessageItem updated: memoization added

--- src/components/MessageList/MessageItem/MessageItem.tsx
+++ src/components/MessageList/MessageItem/MessageItem.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, memo } from 'react';
 import { Message } from '../../../types.d';
 
 interface MessageItemProps {
@@ -6,18 +6,23 @@ interface MessageItemProps {
   alignment?: 'left' | 'right';
 }
 
-const MessageItem: FC<MessageItemProps> = ({ message: { message, author, datetime }, alignment }) => {
-  return (
-    <div>
-      <div className={`d-flex mb-1 justify-content-between ${alignment === 'right' ? 'flex-row-reverse' : ''}`}>
-        <div>{author}</div>
-        <div className='text-muted'>{datetime.toFormat('ff')}</div>
+const MessageItem: FC<MessageItemProps> = memo(
+  ({ message: { message, author, datetime }, alignment }) => {
+    console.log(message);
+
+    return (
+      <div>
+        <div className={`d-flex mb-1 justify-content-between ${alignment === 'right' ? 'flex-row-reverse' : ''}`}>
+          <div>{author}</div>
+          <div className='text-muted'>{datetime.toFormat('ff')}</div>
+        </div>
+        <div className='d-flex ms-3 mb-3 justify-content-start'>
+          <p className='small p-2 rounded-3 text-start bg-body-tertiary'>{message}</p>
+        </div>
       </div>
-      <div className='d-flex ms-3 mb-3 justify-content-start'>
-        <p className='small p-2 rounded-3 text-start bg-body-tertiary'>{message}</p>
-      </div>
-    </div>
-  );
-};
+    );
+  },
+  ({ message: prev }, { message: next }) => prev._id === next._id
+);
 
 export default MessageItem;
